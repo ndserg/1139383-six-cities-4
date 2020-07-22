477bf33d4cb45b17625bfe2a33a746732f14e472
ndserg <ndserg@gmail.com>
2020-07-22 16:25:45 +0300

    испоавляет путь в тесте Property на property

--- src/components/property/property.test.js
+++ src/components/property/property.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Property from "./Property";
+import Property from "./property";
 import {OFFERS} from "../../mocks/offers.js";
 
 it(`Render Property`, () => {
